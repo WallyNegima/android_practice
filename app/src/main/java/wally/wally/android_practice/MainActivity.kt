92d61805035ed01598a7699eb14ebb37165ab188
wally <u.str.gm@gmail.com>
2020-06-05 22:25:25 +0900

    groupieでリスト表示

--- app/src/main/java/wally/wally/android_practice/MainActivity.kt
+++ app/src/main/java/wally/wally/android_practice/MainActivity.kt
@@ -3,6 +3,9 @@ package wally.wally.android_practice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
 import wally.wally.android_practice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +13,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val adapter = GroupAdapter<GroupieViewHolder>()
+        binding.recyclerView.adapter = adapter
+        binding.lifecycleOwner = this
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        adapter.update((1..30).map { TopListItem("テストボタン${it}") })
+        adapter.add(TopListItem("テストボタンファイナル"))
 
         binding.fab.setOnClickListener {
-           // TODO: code
+            // TODO: code
         }
     }
 }

29163aa8a974c8088fc7dd8eb1aba2daa9339a79
wally <u.str.gm@gmail.com>
2020-09-12 17:46:22 +0900

    show toast

--- app/src/main/java/wally/wally/android_practice/views/jetpackCompose/JetpackComposeActivity.kt
+++ app/src/main/java/wally/wally/android_practice/views/jetpackCompose/JetpackComposeActivity.kt
@@ -1,17 +1,30 @@
 package wally.wally.android_practice.views.jetpackCompose
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 
 class JetpackComposeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("Hello world!")
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Greeting("Hello world!")
+                Toaster(context = applicationContext)
+            }
         }
     }
 }
@@ -19,6 +32,16 @@ class JetpackComposeActivity : AppCompatActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+
+
+@Composable
+fun Toaster(context: Context) {
+    Button(onClick = {
+        Toast.makeText(context, "You just clicked a Clickable", Toast.LENGTH_LONG).show()
+    }) {
+        Text("Show Toast!")
+    }
 }
 
 @Preview
